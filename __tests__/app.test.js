83ddc78e361c03e990ce85766f10f217c2c4ccfe
Diana Nita <diana.nita210@gamil.com>
2022-11-14 14:44:25 +0000

    create test file for app.js where the GET topics is tested with error handling supertest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const db = require('../db/connection.js');
 const seed  =require('../db/seeds/seed.js');
 const testData = require('../db/data/test-data/index.js');
 
+
 beforeAll(() => {
     return seed(testData);
 })
