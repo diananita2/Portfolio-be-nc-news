1700230939c424d10048a24e48c87feae5234279
Diana Nita <diana.nita210@gamil.com>
2022-11-14 14:46:43 +0000

    create controllers folder where we send the response of the request

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,6 +1,7 @@
 const { fetchTopics } = require("../models/topics");
 
 exports.getTopics = (req,res,next) => {
+    
     fetchTopics().then((result) => {
        res.status(200).send({topics:result});
     }).catch((err) => {
