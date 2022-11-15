6ba1caa2e0ce152d53e4dba4d44a14e9cd2b1630
Diana Nita <diana.nita210@gamil.com>
2022-11-15 11:50:35 +0000

    invoked the function that gets the comments from controllers

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getArticles } = require('./controllers/articles');
+const { getArticles, getArticleById, getCommentsByArticleId } = require('./controllers/articles');
 const { getTopics} = require('./controllers/topics');
 
 const app = express();
@@ -10,9 +10,19 @@ app.get('/api/topics',getTopics);
 
 app.get('/api/articles',getArticles);
 
+app.get('/api/articles/:article_id',getArticleById);
+
+app.get('/api/articles/:article_id/comments',getCommentsByArticleId);
+
 app.use((err,req,res,next) => {
     if(err.status && err.msg) {
         res.status(err.status).send({msg:err.msg})
     }
+    else{
+        next(err)
+    }
+})
+app.use((err,req,res,next) => {
+    res.status(500).send({msg:'server error!'})
 })
 module.exports = app;
