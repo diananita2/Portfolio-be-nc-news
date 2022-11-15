11e061b4682f24ce5f0a7a54cbe1f58a5bb1cd46
Diana Nita <diana.nita210@gamil.com>
2022-11-15 11:53:44 +0000

    create function in models folder that request data from sql table

--- models/articles.js
+++ models/articles.js
@@ -1,4 +1,5 @@
 const db = require('../db/connection.js');
+const { checkArticleExists } = require('../db/utils.js');
 
 exports.fetchArticles = () => {
     
@@ -10,3 +11,47 @@ exports.fetchArticles = () => {
         return result.rows;
     });
 }
+
+exports.fetchArticleById = (article_id) => {
+    
+    if(!article_id.match(/^[0-9]*$/gm)){
+        return Promise.reject({
+            status: 400,
+            msg: 'invalid request for article id'
+        })
+    }
+    return db.query(
+        `
+        SELECT * FROM articles
+        WHERE article_id = $1;
+        `,[article_id]
+    ).then((result) => {
+        if(result.rows.length === 0){
+            return Promise.reject({
+                status: 404,
+                msg: 'invalid request for article id'
+            })
+        }
+        return result.rows[0];
+    });
+}
+
+exports.fetchCommentsByArticleId = (article_id) => {
+    
+    if(!article_id.match(/^[0-9]*$/gm)){
+        return Promise.reject({
+            status: 400,
+            msg: 'invalid request for article id'
+        })
+    }
+    return checkArticleExists(article_id)
+        .then(() => {
+        return db.query(`
+            SELECT * FROM comments
+            WHERE article_id = $1;
+            `,[article_id])
+        })
+        .then((result) => {
+            return result.rows;
+        })
+    }
