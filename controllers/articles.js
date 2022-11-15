a044c34166f1f25e1d2cbb111a9a28d4bc8baa8d
Diana Nita <diana.nita210@gamil.com>
2022-11-15 11:53:12 +0000

    create function in controllers folder that send the response

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,9 +1,28 @@
-const { fetchArticles } = require("../models/articles");
+const { fetchArticles, fetchArticleById, fetchCommentsByArticleId } = require("../models/articles");
 
 exports.getArticles = (req,res,next) => {
 
     fetchArticles().then((result) => {
        res.status(200).send({articles:result});
+    }).catch((err) => {
+        next(err);
+    });
+};
+
+exports.getArticleById = (req,res,next) => {
+    const {article_id} = req.params;
+    fetchArticleById(article_id).then((result) => {
+       res.status(200).send({article:result});
+    }).catch((err) => {
+        next(err);
+    });
+};
+
+exports.getCommentsByArticleId = (req,res,next) => {
+    const {article_id} = req.params;
+  
+    fetchCommentsByArticleId(article_id).then((result) => {
+        res.status(200).send({comments:result});
     }).catch((err) => {
         next(err);
     });
